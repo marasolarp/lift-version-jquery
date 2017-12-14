7123754f9e6122bec1a654e6fa6d4665813e351a
marasolarp <mariasolarp@gmail.com>
2017-12-14 16:04:05 -0300

    3 pantallas con sus respectivos next y back

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,40 @@
 $(document).ready(function() {
 /*splash*/
 	$('.login').hide();
+	$('.check, .verify, .register').hide();
 	setTimeout(function(){
 		$('#init-lift').fadeOut(500);
 		$('.login').show();
 	}, 2000);
+
+	$('.check, .verify').hide();//ocultar seccion de sing-up
+
+	$('#sing-up').click(function(){//activar el boton que me muestre la siguiente pantalla
+		$('.login, .verify, .register').hide();
+		$('.check').show();
+	});
+
+	$('.btn-icons').click(function(){//volver al login
+		$('.login').show();
+		$('.check, .verify').hide();
+	});
+	$('#btn-near').click(function(){//activar el boton que me muestre la siguiente pantalla
+		$('.login, .check').hide();
+		$('.verify').show();
+	});
+
+	$('.btn-icons1').click(function(){//volver al login
+		$('.check').show();
+		$('.verify').hide();
+	});
+	$('#btn-verify').click(function(){//activar el boton que me muestre la siguiente pantalla
+		$('.check, .verify').hide();
+		$('.register').show();
+	});
+
+	$('.btn-icons2').click(function(){//volver al login
+		$('.verify').show();
+		$('.register').hide();
+	});
 });
 
